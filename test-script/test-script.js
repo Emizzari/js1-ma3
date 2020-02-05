4c6708c34369f6a1f7e3a3fc665553a721b60337
Emizzari <emilie@tonnessen.com>
2020-02-05 10:49:21 +0100

    Test files updated

--- test-script/test-script.js
+++ test-script/test-script.js
@@ -1,11 +1,90 @@
-//  QUESTION 1
-/*  Refer: lesson 1
-    Convert the function below to an arrow function:
+//  QUESTION 2
+/*  Refer: lesson 2 / 4
+    Make a call to the URL below, pass the JSON it returns to a function
+    and inside that function loop through the results and log each
+    elephant's name.
 
-    function(a, b) {
-        return a - b;
-    } */
+    In the "catch" method of your code, redirect to "error.html" if there
+    is an error.
+
+    `https://elephant-api.herokuapp.com/elephants`
+        < div class="results" ></div > */
+
+/*
+const elephantUrl = "https://elephant-api.herokuapp.com/elephants";
+
+const elephantNamesFromUrl = `${elephantUrl}elephantNames`;
+
+fetch(elephantNamesFromUrl)
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(json) {
+        loadElephants(json);
+    })
+
+    .catch(function(error){
+        console.log("error.html");
+    }) */
+
+function loadGames(json) {
+    const games = json.results;
+}
+
+const baseUrl = "https://api.rawg.io/api/";
+const gamesUrl = `${baseUrl}games`;
 
 
-(a, b) => { return a - b; }
+fetch(gamesUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        loadGames(json);
+    })
+    .catch(function (error) {
+        console.dir(error);
+    });
+
+function loadPage(event) {
+    console.log(this.dataset.genre);
+}
+
+
+
+    
+
+
+    
+
+    /*
+const baseUrl = "https://api.rawg.io/api/";
+const gamesUrl = `${baseUrl}games`;
+
+fetch(gamesUrl)
+    .then(function (response) {
+
+        return response.json();
+
+    })
+
+    .then(function (json) {
+
+        loadGames(json);
+
+    })
+
+    .catch(function (error) {
+
+        console.dir(error);
+
+    });
+
+function loadGames(json) {
+    console.dir(json);
+    const games = json.results;
+}
+
+*/
 
