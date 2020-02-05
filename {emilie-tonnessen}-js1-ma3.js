fb3016543742d76c169fa5053b130a2c8efd5876
Emizzari <emilie@tonnessen.com>
2020-02-05 11:50:47 +0100

    Q2 description updated

--- {emilie-tonnessen}-js1-ma3.js
+++ {emilie-tonnessen}-js1-ma3.js
@@ -12,12 +12,10 @@
     
 //  QUESTION 2
 /*  Refer: lesson 2 / 4
-    Make a call to the URL below, pass the JSON it returns to a function 
-    and inside that function loop through the results and log each 
-    elephant's name.
+    Make a call to the URL below, pass the JSON it returns to a function
+    and inside that function loop through the results and log each game's name.
 
-    In the "catch" method of your code, redirect to "error.html" if there 
-    is an error.
+    In the catch method of your code, redirect to error.html if there is an error.
 
     `https://elephant-api.herokuapp.com/elephants`    */
 
