0abc0d40be8d98ceb22aa13d8c4cf7da1c443ff7
Emizzari <emilie@tonnessen.com>
2020-02-06 09:52:32 +0100

    Update test-script.js

--- test-script/test-script.js
+++ test-script/test-script.js
@@ -1,76 +1,98 @@
-//  QUESTION 2
-/*  Refer: lesson 2 / 4
-    Make a call to the URL below, pass the JSON it returns to a function 
-    and inside that function loop through the results and log each game's name.
+//  QUESTION 4
+/*  Refer: lesson 4
+    Given the URL below, write code that checks if there is a userId 
+    parameter in the query string.
 
-    In the catch method of your code, redirect to error.html if there is an error.
+    If there is no userID parameter, redirect the browser to third.html.
 
-    `https://elephant-api.herokuapp.com/elephants`    */
+    If there is a userID parameter and its value is less than 10, 
+    redirect to first.html.
+
+    If there is a userID parameter and its value is 10 or greater, 
+    redirect to second.html.
+
+    https://my.site.com?userId=7      */
+
+    /*const url = "https://my.site.com?userId=7";
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (json) {
+            loadGames(json);
+        })
+
+        .catch(function (error) {
+            console.dir(error);
+
+            
+        }); 
+
+
+    const queryString = document.location.search;
+    const params = new URLSearchParams(queryString);
+
+    let id;
+
+    if (params.has("id")) {
+        id = params.get("id");
+    } else {
+      //  document.location.href = " ";
+    }
 
 
     
 
-function loadGames(json) {
-    if (json.results) {
-        for (let i in json.results) {
-            console.log(json.results[i].name);
-        }
-    } 
+
+//  QUESTION 5
+/*  Refer: lesson 4
+    Write code that removes the button, and only the button, from its 
+    parent element in the HTML below:
+
+    <div class="container">
+        <p>Lorem ipsum dolor</p>
+        <button class="btn">Click me</button>
+    </div>                                                     */
+
+    
+
+const container = document.querySelector(".container");
+const button = document.querySelector(".btn");
+
+container.removeChild(button);
+
+
+//  QUESTION 6
+/*  Refer: lesson 4
+    Create an li element with a text value of Parrots and a class of 
+    parrots.
+
+    Add the new item as the second item in the ul below(add it after Cows).
+
+    < ul class="animals" >
+        <li class="cows">Cows</li>
+        <li class="elephants">Elephants</li>
+    </ul >                                             */
+
+    
+
+
+//  QUESTION 7
+/*  Refer: lesson 4
+    Make a call to the URL below and pass the JSON it returns to a 
+    function.
+
+    Inside that function select the div from the HTML below and assign 
+    the rating property from the JSON object as it's text value.
+
+    In the catch method, log the error that may be returned.
+
+    `https://api.rawg.io/api/games/3801`
+        < div class="rating" ></div >                          */
+
+function testFunctionHere() {
+    console.log(answer);
 }
-
-const url = "https://api.rawg.io/api/games?genres=sports";
-
-fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-
-    .then(function (json) {
-        loadGames(json);
-    })
-
-    .catch(function (error) {
-        console.dir(error);
-
-        document.location.href = "error.html";
-    });
-
-
-
-
-
-    
-
-
-    
-
-    /*
-const baseUrl = "https://api.rawg.io/api/";
-const gamesUrl = `${baseUrl}games`;
-
-fetch(gamesUrl)
-    .then(function (response) {
-
-        return response.json();
-
-    })
-
-    .then(function (json) {
-
-        loadGames(json);
-
-    })
-
-    .catch(function (error) {
-
-        console.dir(error);
-
-    });
-
-function loadGames(json) {
-    console.dir(json);
-    const games = json.results;
-}
-
-*/
 
