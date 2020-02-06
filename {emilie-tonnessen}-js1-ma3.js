d52417956fb41b30599e648d81b84b7f10d90991
Emizzari <emilie@tonnessen.com>
2020-02-06 10:09:28 +0100

    Q6 - added answer
    Code is tested and finished. Everything works fine. DOM is being changed correctly to the task description and no errors are being displayed.

--- {emilie-tonnessen}-js1-ma3.js
+++ {emilie-tonnessen}-js1-ma3.js
@@ -115,9 +115,21 @@
         <li class="elephants">Elephants</li>
     </ul >                                             */
 
-    function testFunctionHere() {
-        console.log(answer);
-    }
+    const animalsContainer = document.querySelector(".animals");
+
+    const cows = document.querySelector(".cows");
+
+    const parrots = document.createElement("li");
+
+    parrots.className = "parrots";
+
+    const liContent = document.createTextNode("Parrots");
+
+    parrots.appendChild(liContent);
+
+    animalsContainer.appendChild(parrots);
+
+    cows.after(parrots);
 
 
 //  QUESTION 7
