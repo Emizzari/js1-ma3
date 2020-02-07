0af5edd70818ed1553dd7e89d3e6229b9168ef38
Emizzari <emilie@tonnessen.com>
2020-02-07 11:52:52 +0100

    Updating variable names and question description
    Updating to more fitting and logical names

--- {emilie-tonnessen}-js1-ma3.js
+++ {emilie-tonnessen}-js1-ma3.js
@@ -1,7 +1,7 @@
 //  JAVASCRIPT 1 - MODULE ASSIGNMENT 3
 //  QUESTION 1
 /*  Refer: lesson 1
-    Convert the function below to an arrow function:
+    - Convert the function below to an arrow function:
 
     function(a, b) {
         return a - b;
@@ -12,12 +12,13 @@
     
 //  QUESTION 2
 /*  Refer: lesson 2 / 4
-    Make a call to the URL below, pass the JSON it returns to a function
-    and inside that function loop through the results and log each game's name.
+    - Make a call to the URL below, pass the JSON it returns to a function and inside 
+      that function loop through the results and log each game's name.
+    - In the catch method of your code, redirect to error.html if there is an error.
 
-    In the catch method of your code, redirect to error.html if there is an error.
+    https://api.rawg.io/api/games?genres=sports */
 
-    `https://elephant-api.herokuapp.com/elephants`    */
+    const url = "https://api.rawg.io/api/games?genres=sports";
 
     function loadGames(json) {
         if (json.results) {
@@ -26,8 +27,6 @@
             }
         }
     }
-
-    const url = "https://api.rawg.io/api/games?genres=sports";
 
     fetch(url)
         .then(function (response) {
@@ -47,27 +46,24 @@
 
 //  QUESTION 3
 /*  Refer: lesson 3
-    Replace the word cats with the word giraffes in the following sentence:
+    - Replace the word cats with the word giraffes in the following sentence:
 
-    "These cats are outrageous."                                     */
+    "These cats are outrageous." */
 
-    let sentenceOutrageousCats = "These cats are outrageous.";
+    let sentence = "These cats are outrageous.";
 
-    var changeCatsToGiraffes = sentenceOutrageousCats.replace(/cats/g, "giraffes");
+    var replaceCatsWithGiraffes = sentence.replace(/cats/g, "giraffes");
 
 
 //  QUESTION 4
 /*  Refer: lesson 4
-    Given the URL below, write code that checks if there is a userId 
-    parameter in the query string.
-
-    If there is no userID parameter, redirect the browser to third.html.
-
-    If there is a userID parameter and its value is less than 10, 
-    redirect to first.html.
-
-    If there is a userID parameter and its value is 10 or greater, 
-    redirect to second.html.
+    - Given the URL below, write code that checks if there is a userId 
+      parameter in the query string.
+    - If there is no userID parameter, redirect the browser to third.html.
+    - If there is a userID parameter and its value is less than 10, 
+      redirect to first.html.
+    - If there is a userID parameter and its value is 10 or greater, 
+      redirect to second.html.
 
     https://my.site.com?userId=7 */
 
@@ -91,13 +87,13 @@
         
 //  QUESTION 5
 /*  Refer: lesson 4
-    Write code that removes the button, and only the button, from its 
-    parent element in the HTML below:
+    - Write code that removes the button, and only the button, from its parent 
+      element in the HTML below:
 
     <div class="container">
         <p>Lorem ipsum dolor</p>
         <button class="btn">Click me</button>
-    </div>                                                     */
+    </div> */
 
     const container = document.querySelector(".container");
     const button = document.querySelector(".btn");
@@ -107,15 +103,13 @@
 
 //  QUESTION 6
 /*  Refer: lesson 4
-    Create an li element with a text value of Parrots and a class of 
-    parrots.
-
-    Add the new item as the second item in the ul below(add it after Cows).
+    - Create an li element with a text value of Parrots and a class of parrots.
+    - Add the new item as the second item in the ul below(add it after Cows).
 
     < ul class="animals" >
         <li class="cows">Cows</li>
         <li class="elephants">Elephants</li>
-    </ul >                                             */
+    </ul > */
 
     const animalsContainer = document.querySelector(".animals");
 
@@ -138,15 +132,12 @@
 /*  Refer: lesson 4
     - Make a call to the URL below 
     - and pass the JSON it returns to a function.
-
     - Inside that function select the div from the HTML below 
     - and assign the "rating" property FROM the JSON object as it's text value.
-
     - In the catch method, log the error that may be returned.
 
-    https://api.rawg.io/api/games/3801
-
-    < div class="rating" ></div >  */
+        https://api.rawg.io/api/games/3801
+        < div class="rating" ></div > */
 
     // Making a call to the URL:
     const GamesUrl = "https://api.rawg.io/api/games/3801";
