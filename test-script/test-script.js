8d9b37ca2a36b22da8595c566f43db4f98fb82fc
Emizzari <emilie@tonnessen.com>
2020-02-07 13:19:43 +0100

    Update test-script.js

--- test-script/test-script.js
+++ test-script/test-script.js
@@ -1,51 +1,16 @@
-//  QUESTION 7
-/*  Refer: lesson 4
-    - Make a call to the URL below 
-    - and pass the JSON it returns to a function.
+const animalsContainer = document.querySelector(".animals");
 
-    - Inside that function select the div from the HTML below 
-    - and assign the "rating" property FROM the JSON object as it's text value.
+const cows = document.querySelector(".cows");
 
-    - In the catch method, log the error that may be returned.
+const parrots = document.createElement("li");
 
-    https://api.rawg.io/api/games/3801
+parrots.className = "parrots";
 
-    < div class="rating" ></div >  */
+const liContent = document.createTextNode("Parrots");
 
+parrots.appendChild(liContent);
 
+animalsContainer.appendChild(parrots);
 
-    
-    // Making a call to the URL:
-    const GamesUrl = "https://api.rawg.io/api/games/3801";
-
-    // Creating JSON (??):
-    fetch(GamesUrl)
-        .then(function (response) {
-            return response.json();
-        })
-
-        .then(function (json) {
-            createRating(json);
-        })
-
-        // Logging an error that may be returned:
-        .catch(function (error) {
-            console.dir(error);
-        });
-
-    // Creating the function
-    function createRating(json) {
-
-        // Selecting the <div> element from HTML document:
-        const ratingContainer = document.querySelector(".rating");
-
-        // Create a <p> element to contain the ratings (for personal preference):
-        const rating = document.createElement("p");
-
-        // Adding the <p> element after the <div>
-        ratingContainer.appendChild(rating);
-
-        // Adding text and rating property from the JSON object:
-        rating.innerText = `Rating: ${json.rating}`;
-    }
+cows.after(parrots);
 
